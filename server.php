fcdbee58b3678bdd40b4e354d0eebcfeaf6cb1df
llr <suga>
2023-07-23 18:02:30 +0800

    gichiinii ur sad

--- server.php
+++ server.php
@@ -2,38 +2,44 @@
 session_start();
 
 // initializing variables
-$username = "";
-$email    = "";
+$email = "";
 $errors = array(); 
 
-// xacbank.database.windows.net
 // connect to the database
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, "./DigiCertTLSECCP384RootG5.crt.pem", NULL, NULL);
+$db = mysqli_real_connect($conn, 'xacbank.database.windows.net', 'sugallr', 'mkN@C92chYS7vjU', 'xac_fuck_up', 3306, NULL, MYSQLI_CLIENT_SSL);
 
-$db = mysqli_connect('xacbank.database.windows.net', 'sugallr', 'mkN@C92chYS7vjU', 'xac_fuck_up');
-// $db = mysqli_connect('localhost', 'root', '', 'xac_fuck_up');
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
-  $email = mysqli_real_escape_string($db, $_POST['email']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
 
-  if (empty($email)) { array_push($errors, "Email is required"); }
-
-  $user_check_query = "SELECT * FROM SUGA_LLR WHERE email='$email' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-  
-  if ($user) { // if user exists
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "Invalid email format");
     }
-  }
 
-  if (count($errors) == 0) {
-  	$query = "INSERT INTO SUGA_LLR (email) 
-  			  VALUES('$email')";
-  	mysqli_query($db, $query);
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
-  }
+    $user_check_query = "SELECT * FROM SUGA_LLR WHERE email='$email' LIMIT 1";
+    $result = mysqli_query($conn, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+  
+    if ($user) { // if user exists
+        if ($user['email'] === $email) {
+            array_push($errors, "Email already exists");
+        }
+    }
+
+    if (count($errors) == 0) {
+        $query = "INSERT INTO SUGA_LLR (email) 
+                  VALUES('$email')";
+        mysqli_query($conn, $query);
+        $_SESSION['success'] = "You are now registered";
+        header('location: index.php');
+    }
 }
 ?>
